c8922132196dff87fe58cc03dba848dcc17c3848
Anon <you@example.com>
2026-05-19 16:32:57 +0200

    Base website complete

--- blog/2_Vibe Coding.md
+++
@@ -1,4 +0,0 @@
-# Which one vibes?
-You are a developer. You are creating a website using only static HTML, CSS and JS. You are Using the bootstrap 5.0.0 library. Create code for a navbar with a back button and three links, the first called Home, the second called Map and the third called Blog.
-
-- perplexity used an absolute link while chatgpt used a relative link
--- js/map.js
+++ js/map.js
@@ -7,31 +7,43 @@ $(document).ready(function() {
 	});
 
 	$('#France').popover({
-		content: '<ul class="list-group"><li class="list-group-item list-group-item-success">2020 Tweag I/O</li><li class="list-group-item">Sales and Marketing Engineer</li><li class="list-group-item active">2019 HEC Paris</li><li class="list-group-item">MBA</br>HEC Excellence Scholar</li></ul>',
+		content: '<ul class="list-group"><li class="list-group-item list-group-item-warning">2022-present<br> Université PSL Dauphine</li><li class="list-group-item">Adjunct lecture in Solidity and Smart Contract programming.<br>Uses cases on CBDCs, Supply Chain Management and DAOs.</li><li class="list-group-item list-group-item-success">2020-2021<br> Tweag I/O</li><li class="list-group-item">Sales and Marketing Engineer<br>Haskell, Nix, Bazel.</li><li class="list-group-item active">2019-2020<br>HEC Paris</li><li class="list-group-item">MBA with Digital Innovation specialisation.</br>HEC Excellence Scholar.</li></ul>',
+		html: true,
+		trigger: 'hover'
+	});
+
+	$('#Munich').popover({
+		content: '<ul class="list-group"><li class="list-group-item list-group-item-success">2024-2025<br>Universität der Bundeswehr</li><li class="list-group-item">Blockchain Researcher<br>Blockchain-enabled Digital Twins of EEE-components in the Space supply chain.</li></ul>',
+		html: true,
+		trigger: 'hover'
+	});
+
+	$('#Berlin').popover({
+		content: '<ul class="list-group"><li class="list-group-item list-group-item-success">2022 - 2023<br> Celo Foundation</li><li class="list-group-item">University Partnerships<br>Hubspot customization and management. <br>Conference and event management.</li></ul>',
 		html: true,
 		trigger: 'hover'
 	});
 
 	$('#China').popover({
-		content: '<ul class="list-group"><li class="list-group-item list-group-item-info">1993 Born Here</li></ul>',
+		content: '<ul class="list-group"><li class="list-group-item list-group-item-info">1993</li><li class="list-group-item">Birthplace</li></ul>',
 		html: true,
 		trigger: 'hover'
 	});
 
 	$('#US').popover({
-		content: '<ul class="list-group"><li class="list-group-item active">2012 Duke University</li><li class="list-group-item">B.Eng in Electrical and Computer Engineering</br>Mastercard Foundation Scholar</li></ul>',
+		content: '<ul class="list-group"><li class="list-group-item active">2012-2016 <br> Duke University</li><li class="list-group-item">B.Eng in Electrical and Computer Engineering</br>Mastercard Foundation Scholar</li></ul>',
 		html: true,
 		trigger: 'hover'
 	});
 
 	$('#MiddleEast').popover({
-		content: '<ul class="list-group"><li class="list-group-item list-group-item-success">2018 IBM</li><li class="list-group-item">Client Technical Professional for SaaS Application Security, Middle East and Africa region</li><</ul>',
+		content: '<ul class="list-group"><li class="list-group-item list-group-item-success">2017-2018 <br>IBM MEA</li><li class="list-group-item">Client Technical Professional for Application, Mobile and Endpoint security. <br>Proof-of-Concepts, enablement, technical customer success.</li><</ul>',
 		html: true,
 		trigger: 'hover'
 	});
 
 	$('#SouthAfrica').popover({
-		content: '<ul class="list-group"><li class="list-group-item list-group-item-success">2016 IBM</li><li class="list-group-item">Client Technical Professional in Cyber Security for Africa</li><li class="list-group-item active">2010 African Leadership Academy</li><li class="list-group-item">High School for Africans with leadership potential</br>A-levels</li></ul>',
+		content: '<ul class="list-group"><li class="list-group-item list-group-item-success">2016-2017<br>IBM South Africa</li><li class="list-group-item">Client Technical Professional for Mobile and Endpoint security.<br>Channel and key accounts.<br>IBM Sales Academy.</li><li class="list-group-item active">2010 - 2012 <br> African Leadership Academy</li><li class="list-group-item">Pan-African High School</br>A-levels in Sciences</li></ul>',
 		html: true,
 		trigger: 'hover'
 	});
